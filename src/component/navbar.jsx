4ce44f0a1a7ffdbc71dba1e76f26e512e45c7e0b
YenaLey <yena.e121@gmail.com>
2024-08-13 21:07:50 +0900

    Feat: 배포 시 navbar 깨짐 해결

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -5,8 +5,8 @@ import { useNavigate } from 'react-router-dom';
 import '../App.css';
 import Logo from './logo';
 
-function QuipuDevDropdown({ quipudevDropdown }) {
-  return quipudevDropdown ? (
+function QuipuDevDropdown({ isVisible }) {
+  return isVisible ? (
     <div className="dropdown-box">
       <NavLink to="/quipu-dev" smooth>
         <p>Showcase</p>
@@ -18,14 +18,8 @@ function QuipuDevDropdown({ quipudevDropdown }) {
   ) : null;
 }
 
-function JoinQuipuDropdown() {
-  const navigate = useNavigate();
-
-  const handleNavigation = (page) => {
-    navigate('/join-quipu', { state: { selectedPage: page } });
-  };
-
-  return (
+function JoinQuipuDropdown({ isVisible, onNavigate }) {
+  return isVisible ? (
     <div className="dropdown-box">
       <p
         style={{
@@ -33,7 +27,7 @@ function JoinQuipuDropdown() {
           color: '#567FBB',
           backgroundColor: '#EDF4FF',
         }}
-        onClick={() => handleNavigation('general')}
+        onClick={() => onNavigate('general')}
       >
         일반 부원
       </p>
@@ -43,57 +37,38 @@ function JoinQuipuDropdown() {
           color: '#567FBB',
           backgroundColor: '#EDF4FF',
         }}
-        onClick={() => handleNavigation('development')}
+        onClick={() => onNavigate('development')}
       >
         개발 부원
       </p>
     </div>
-  );
+  ) : null;
 }
 
 const Navbar = () => {
-  const [quipudevDropdown, setQuipudevDropdown] = useState(false);
-  const [joinquipuDropdown, setJoinquipuDropdown] = useState(false);
-  const [isJoinQuipuVisible, setIsJoinQuipuVisible] = useState(false);
-  const [isActivityDetailVisible, setIsActivityDetailVisible] = useState(false);
+  const [dropdownState, setDropdownState] = useState({
+    quipuDevDropdown: false,
+    joinQuipuDropdown: false,
+    activityToggle: false,
+    quipuDevToggle: false,
+    joinQuipuToggle: false,
+  });
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const handleClickOutside = (event) => {
-    if (!event.target.closest('.navbar__menu--pc') && !event.target.closest('.dropdown-box')) {
-      setQuipudevDropdown(false);
-      setJoinquipuDropdown(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside);
-    return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, []);
-
-  const toggleActivityDetail = () => {
-    setIsActivityDetailVisible(!isActivityDetailVisible);
-  };
-
-  const toggleJoinQuipu = () => {
-    setIsJoinQuipuVisible(!isJoinQuipuVisible);
-  };
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+  const toggleDropdown = (dropdown) => {
+    setDropdownState((prevState) => ({
+      ...prevState,
+      [dropdown]: !prevState[dropdown],
+    }));
   };
 
   const closeMenu = () => {
     setMenuOpen(false);
   };
 
-  const closeMenu_joinQuipu_general = () => {
-    setMenuOpen(false);
-  };
-
-  const closeMenu_joinQuipu_development = () => {
-    setMenuOpen(false);
+  const navigate = useNavigate();
+  const handleNavigation = (page) => {
+    navigate('/join-quipu', { state: { selectedPage: page } });
   };
 
   function setScreenSize() {
@@ -113,52 +88,52 @@ const Navbar = () => {
 
       {/* pc에서 메뉴들 */}
       <div className="navbar__menu--pc">
-        <ul>
-          <li>
-            <Link to="home" smooth={true} duration={100}>
-              home
-            </Link>
-          </li>
-          <li>
-            <Link to="about" smooth={true} duration={100}>
-              about
-            </Link>
-          </li>
-          <li>
-            <Link to="activity" smooth={true} duration={100}>
-              activity
-            </Link>
-          </li>
-          <li>
-            <Link to="recommend-site" smooth={true} duration={100}>
-              recommend site
-            </Link>
-          </li>
-          <h4
-            onClick={() => {
-              setQuipudevDropdown(!quipudevDropdown);
-            }}
-          >
-            quipu dev
-            <QuipuDevDropdown quipudevDropdown={quipudevDropdown} />
-          </h4>
-          <h4
-            style={{
-              border: '1.5px solid rgb(86, 127, 187, 0.8)',
-              color: '#567FBB',
-            }}
-            onClick={() => {
-              setJoinquipuDropdown(!joinquipuDropdown);
-            }}
-          >
-            join quipu
-            {joinquipuDropdown && <JoinQuipuDropdown />}
-          </h4>
-        </ul>
+        <li>
+          <Link to="home" smooth={true} duration={100}>
+            home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={100}>
+            about
+          </Link>
+        </li>
+        <li>
+          <Link to="activity" smooth={true} duration={100}>
+            activity
+          </Link>
+        </li>
+        <li>
+          <Link to="recommend-site" smooth={true} duration={100}>
+            recommend site
+          </Link>
+        </li>
+        <h4 onClick={() => toggleDropdown('quipuDevDropdown')}>
+          quipu dev
+          <QuipuDevDropdown isVisible={dropdownState.quipuDevDropdown} />
+        </h4>
+        <h4
+          style={{
+            border: '1.5px solid rgb(86, 127, 187, 0.8)',
+            color: '#567FBB',
+          }}
+          onClick={() => toggleDropdown('joinQuipuDropdown')}
+        >
+          join quipu
+          <JoinQuipuDropdown
+            isVisible={dropdownState.joinQuipuDropdown}
+            onNavigate={handleNavigation}
+          />
+        </h4>
       </div>
 
       {/* mobile에서 메뉴 버튼 */}
-      <input id="menu-toggle" type="checkbox" checked={menuOpen} onChange={toggleMenu} />
+      <input
+        id="menu-toggle"
+        type="checkbox"
+        checked={menuOpen}
+        onChange={() => setMenuOpen(!menuOpen)}
+      />
       <label className="menu-button" htmlFor="menu-toggle">
         <span className="line line1"></span>
         <span className="line line2"></span>
@@ -167,76 +142,105 @@ const Navbar = () => {
 
       {/* mobile에서 메뉴들 */}
       <div className={`navbar__menu--mobile ${menuOpen ? 'open' : ''}`}>
-        <ul>
-          <li style={{ marginTop: '80px' }}>
-            <Link to="home" smooth={true} duration={100} onClick={closeMenu}>
-              home
+        <li>
+          <Link to="home" smooth={true} duration={100} onClick={closeMenu}>
+            home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={100} onClick={closeMenu}>
+            about
+          </Link>
+        </li>
+        <li>
+          <div className="mobile-dropdown">
+            <Link to="activity" smooth={true} duration={100} onClick={closeMenu}>
+              activity
             </Link>
-          </li>
-          <li>
-            <Link to="about" smooth={true} duration={100} onClick={closeMenu}>
-              about
+            <input
+              style={{ display: 'none' }}
+              id="mobile-dropdown-toggle-1"
+              type="checkbox"
+              onChange={() => toggleDropdown('activityToggle')}
+            />
+            <label className="mobile-dropdown-button" htmlFor="mobile-dropdown-toggle-1">
+              <span className="line line4"></span>
+              <span className="line line5"></span>
+            </label>
+          </div>
+          <div
+            className={`mobile-dropdown-content ${dropdownState.activityToggle ? 'visible' : ''}`}
+          >
+            <NavLink to="/activity-detail#Study" onClick={closeMenu}>
+              <li>study</li>
+            </NavLink>
+            <NavLink to="/activity-detail#Friendship" onClick={closeMenu}>
+              <li>friendship</li>
+            </NavLink>
+            <NavLink to="/activity-detail#MT" onClick={closeMenu}>
+              <li>mt</li>
+            </NavLink>
+          </div>
+        </li>
+        <li>
+          <Link to="recommend-site" smooth={true} duration={100} onClick={closeMenu}>
+            recommend site
+          </Link>
+        </li>
+        <li>
+          <div className="mobile-dropdown">
+            <Link to="quipu-dev" smooth={true} duration={100} onClick={closeMenu}>
+              quipu dev
             </Link>
-          </li>
-          <li>
-            <div className="activity-menu">
-              <Link to="activity" smooth={true} duration={100} onClick={closeMenu}>
-                activity
-              </Link>
-              <input id="activity-toggle" type="checkbox" onChange={toggleActivityDetail} />
-              <label className="activity-button" htmlFor="activity-toggle">
-                <span className="line line4"></span>
-                <span className="line line5"></span>
-              </label>
-            </div>
-            <div className={`activity-detail-menu ${isActivityDetailVisible ? 'visible' : ''}`}>
-              <li style={{ boxShadow: '0px 0px grey' }}>
-                <NavLink to="/activity-detail#Study" onClick={closeMenu}>
-                  &nbsp;&nbsp;&nbsp;&nbsp;study
-                </NavLink>
-              </li>
-              <li style={{ boxShadow: '0px 0px grey' }}>
-                <NavLink to="/activity-detail#Friendship" onClick={closeMenu}>
-                  &nbsp;&nbsp;&nbsp;&nbsp;friendship
-                </NavLink>
-              </li>
-              <li style={{ boxShadow: '0px 0px grey' }}>
-                <NavLink to="/activity-detail#MT" onClick={closeMenu}>
-                  &nbsp;&nbsp;&nbsp;&nbsp;mt
-                </NavLink>
-              </li>
-            </div>
-          </li>
-          <li>
-            <Link to="recommend-site" smooth={true} duration={100} onClick={closeMenu}>
-              recommend site
+            <input
+              style={{ display: 'none' }}
+              id="mobile-dropdown-toggle-2"
+              type="checkbox"
+              onChange={() => toggleDropdown('quipuDevToggle')}
+            />
+            <label className="mobile-dropdown-button" htmlFor="mobile-dropdown-toggle-2">
+              <span className="line line4"></span>
+              <span className="line line5"></span>
+            </label>
+          </div>
+          <div
+            className={`mobile-dropdown-content ${dropdownState.quipuDevToggle ? 'visible' : ''}`}
+          >
+            <NavLink to="/activity-detail#Study" onClick={closeMenu}>
+              <li>study</li>
+            </NavLink>
+            <NavLink to="/activity-detail#Friendship" onClick={closeMenu}>
+              <li>friendship</li>
+            </NavLink>
+          </div>
+        </li>
+        <li>
+          <div className="mobile-dropdown">
+            <Link to="join-quipu" smooth={true} duration={100} onClick={closeMenu}>
+              join quipu
             </Link>
-          </li>
-          <li>
-            <div className="joinquipu-menu">
-              <NavLink to="/join-quipu" onClick={closeMenu}>
-                join quipu
-              </NavLink>
-              <input id="joinquipu-toggle" type="checkbox" onChange={toggleJoinQuipu} />
-              <label className="joinquipu-button" htmlFor="joinquipu-toggle">
-                <span className="line line4"></span>
-                <span className="line line5"></span>
-              </label>
-            </div>
-            <div className={`joinquipu-detail-menu ${isJoinQuipuVisible ? 'visible' : ''}`}>
-              <li style={{ boxShadow: '0px 0px grey' }}>
-                <NavLink to="/join-quipu#general" onClick={closeMenu_joinQuipu_general}>
-                  &nbsp;&nbsp;&nbsp;&nbsp;일반 부원
-                </NavLink>
-              </li>
-              <li style={{ boxShadow: '0px 0px grey' }}>
-                <NavLink to="/join-quipu#development" onClick={closeMenu_joinQuipu_development}>
-                  &nbsp;&nbsp;&nbsp;&nbsp;개발 부원
-                </NavLink>
-              </li>
-            </div>
-          </li>
-        </ul>
+            <input
+              style={{ display: 'none' }}
+              id="mobile-dropdown-toggle-3"
+              type="checkbox"
+              onChange={() => toggleDropdown('joinQuipuToggle')}
+            />
+            <label className="mobile-dropdown-button" htmlFor="mobile-dropdown-toggle-3">
+              <span className="line line4"></span>
+              <span className="line line5"></span>
+            </label>
+          </div>
+          <div
+            className={`mobile-dropdown-content ${dropdownState.joinQuipuToggle ? 'visible' : ''}`}
+          >
+            <NavLink to="/activity-detail#Study" onClick={closeMenu}>
+              <li>study</li>
+            </NavLink>
+            <NavLink to="/activity-detail#Friendship" onClick={closeMenu}>
+              <li>friendship</li>
+            </NavLink>
+          </div>
+        </li>
       </div>
     </nav>
   );
